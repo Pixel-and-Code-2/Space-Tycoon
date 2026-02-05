030368b1822de9723934871d3318d23c8e09a14e
IDPolyakov <Polvan2006@gmail.com>
2026-02-06 02:51:12 +0300

    Add round titles under pawns to detect who is an enemy or a player

---
+++ Assets/Scripts/PawnController/PawnMoveUIController/PawnStatusVisualizer.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class PawnStatusVisualizer : MonoBehaviour
+{
+    [Header("Visuals")]
+    [SerializeField] private Renderer objectRenderer;
+
+    [Header("Colors")]
+    [SerializeField] private Color allyColor = Color.green;
+    [SerializeField] private Color enemyColor = Color.red;
+    [SerializeField] private Color deadColor = Color.gray;
+
+    private SimpleEnemyAI enemyAI;
+    private PawnNavMesh playerAI;
+
+    void Awake()
+    {
+        if (objectRenderer == null) objectRenderer = GetComponent<Renderer>();
+
+        enemyAI = GetComponentInParent<SimpleEnemyAI>();
+        playerAI = GetComponentInParent<PawnNavMesh>();
+    }
+
+    void Start()
+    {
+        UpdateStatusColor();
+    }
+
+    void Update()
+    {
+        UpdateStatusColor();
+    }
+
+    private void UpdateStatusColor()
+    {
+        if (objectRenderer == null) return;
+
+        Color targetColor = Color.white;
+
+        if (enemyAI != null)
+        {
+            if (enemyAI.GetSelectableType() == SelectableType.Dead)
+            {
+                targetColor = deadColor;
+            }
+            else
+            {
+                targetColor = enemyColor;
+            }
+        }
+        else if (playerAI != null)
+        {
+            targetColor = allyColor;
+        }
+
+        if (objectRenderer.material.color != targetColor)
+        {
+            objectRenderer.material.color = targetColor;
+        }
+    }
+}
